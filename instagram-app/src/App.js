e293f51f7b6d49bb017e67fc4feae3cd2cbcbd6f
user <19831942+Jamelle-Boose@users.noreply.github.com>
2019-05-27 13:51:57 -0700

    day 1 almost complete

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -2,15 +2,40 @@ import React from 'react';
 import './App.css';
 import dummyData from './dummy-data'
 import PostContainer from './components/PostContainer/postcontainer'
+import SearchBar from './components/SearchBar/searchbar'
+import { Columns } from "react-bulma-components/full";
+import { Image } from "react-bulma-components/full";
+import { Card } from "react-bulma-components/full";
 
-function App() {
-  return (
-    <div className="App">
-      {dummyData.map(data => (
-        <PostContainer data={data} />
-      ))}
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+  super()
+  this.state = {
+    posts: dummyData
+  } 
+}
+  render() {
+    return (
+      <div className="App">
+        <SearchBar />
+        {dummyData.map(data => (
+          <PostContainer data={data} />
+        ))}
+        <Columns>
+          <Columns.Column size="three-quarters">
+          <Card>
+          <header className="card-header">
+            <p className="card-header-title">
+              Component
+            </p>
+            <Image renderAs="a" href="https://google.com" className="card-header-icon" target="_blank" aria-label="more options" />
+          </header>
+          </Card>
+          </Columns.Column>
+        </Columns>
+      </div>
+    );
+  }
 }
 
 export default App;
